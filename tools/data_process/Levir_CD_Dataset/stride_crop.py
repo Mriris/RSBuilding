69bb4ed5b3443ce2f88f24b6599d634c99b439bd
Mriris <2647381485@qq.com>
2025-01-15 17:08:43 +0800

    支持动态扩展名

--- tools/data_process/Levir_CD_Dataset/stride_crop.py
+++ tools/data_process/Levir_CD_Dataset/stride_crop.py
@@ -1,21 +1,21 @@
 import cv2
-import cv2
 import numpy as np
 import os
 from multiprocessing import Pool
 from tqdm import tqdm
 import argparse
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input-dir",  default="data_dir/Levir-CD/train/A")
+    parser.add_argument("--input-dir", default="data_dir/Levir-CD/train/A")
     parser.add_argument("--output-dir", default="data_dir/Levir-CD/train/A_512_nooverlap")
     return parser.parse_args()
 
 
 def slide_crop(filename):
     real_path = os.path.join(folder_path, filename)
-    name = filename.split('.')[0]
+    name, ext = os.path.splitext(filename)  # 分离文件名和扩展名
     img = cv2.imread(real_path)
     crop_width, crop_height = 512, 512
     step_x, step_y = 512, 512
@@ -26,7 +26,9 @@ def slide_crop(filename):
             crop = img[j:j + crop_height, i:i + crop_width]
             crops.append(crop)
     for i, crop in enumerate(crops):
-        cv2.imwrite(f'{out_path}/{name}_crop_{i}.png', crop)
+        # 使用输入文件的扩展名生成输出文件
+        cv2.imwrite(f'{out_path}/{name}_crop_{i}{ext}', crop)
+
 
 if __name__ == '__main__':
     args = parse_args()
@@ -35,8 +37,7 @@ if __name__ == '__main__':
     os.makedirs(out_path, exist_ok=True)
 
     filenames = [filename for filename in os.listdir(folder_path) if
-                 filename.endswith('.png') or filename.endswith('.jpg') or filename.endswith('.tif')]
-    
+                 filename.endswith('.png') or filename.endswith('.jpg') or filename.endswith('.tif') or filename.endswith('.tiff')]
+
     with Pool(16) as p:
         list(tqdm(p.imap(slide_crop, filenames), total=len(filenames)))
-    
